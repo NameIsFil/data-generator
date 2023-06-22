b37fb7412e14b9b9d76a5bb7a9bf675395dcedb0
NameIsFill <filpopowski@gmail.com>
2023-06-22 02:20:26 +0200

    added correct names

--- src/DataGenerator/DataGenerator.tsx
+++ src/DataGenerator/DataGenerator.tsx
@@ -1,56 +1,71 @@
-import * as styles from "./styles.module.css";
+import * as styles from './styles.module.css';
 import { faker } from '@faker-js/faker';
 
-export function createRandomUser(): User {
+export function createBookingInformation(): User {
     let newUser = {
-        userId: faker.string.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        password: faker.internet.password(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
+        venues: [
+            {
+                id: Math.floor((Math.random()*10)),
+                location: {
+                    postalCode: faker.location.zipCode("##-####"),
+                    name: faker.location.city()
+                },
+                pricePerNightInEUR: faker.commerce.price(),
+                rating: faker.number.float({ precision: 0.1 }),
+                capacity: Math.floor((Math.random()*4)),
+                name: faker.company.name(),
+                albumId: Math.floor((Math.random()*10)),
+            }
+        ],
+        venuesDetails: [
+            {
+                id: Math.floor((Math.random()*300)),
+                venueId: faker.internet.userName(),
+                location: {
+                    postalCode: faker.location.zipCode("##-####"),
+                    name: faker.location.city()
+                },
+                pricePerNightInEUR: faker.commerce.price(),
+                rating: faker.number.float({ min: 1, max: 10, precision: 0.1 }),
+                numberOfReviews: Math.floor((Math.random()*300)),
+                capacity: Math.floor((Math.random()*4)),
+                name: faker.date.past(),
+                albumId: faker.date.past(),
+                description: faker.date.past(),
+                features: [
+                    faker.string.uuid(),
+                    faker.string.uuid(),
+                    faker.string.uuid(),
+                    faker.string.uuid(),
+                    faker.string.uuid()
+                ],
+                sleepingDetails: {
+                    maxCapacity: Math.floor((Math.random()*10)),
+                    amountOfBeds: Math.floor((Math.random()*10))
+                },
+                checkInHour: "15:00",
+                checkOutHour: "12:00",
+                distanceFromCityCenterInKM: faker.internet.email(),
+                contactDetails: {
+                    phone: faker.internet.email(),
+                    email: faker.internet.email()
+                }
+            }
+        ],
     };
     console.log(newUser)
     return newUser
 }
 
-export const USERS: User[] = faker.helpers.multiple(createRandomUser, {
+export const USERS: User[] = faker.helpers.multiple(createBookingInformation, {
     count: 5,
 });
-
 
 export function DataGenerator() {
 
     return (
         <div className={styles.container}>
-            <button onClick={() => createRandomUser()}>Generate</button>
+            <button onClick={createBookingInformation}>Generate</button>
         </div>
     )
-
-    // return (
-    //     <div className={styles.container}>
-    //         <div className={styles.inputField}>
-    //             <p>Name:</p>
-    //             <input placeholder={faker.name.firstName()} />
-    //         </div>
-    //         <div className={styles.inputField}>
-    //             <p>Name:</p>
-    //             <input placeholder={faker.name.firstName()} />
-    //         </div>
-    //         <div className={styles.inputField}>
-    //             <p>Name:</p>
-    //             <input placeholder={faker.name.firstName()} />
-    //         </div>
-    //         <div className={styles.inputField}>
-    //             <p>Name:</p>
-    //             <input placeholder={faker.name.firstName()} />
-    //         </div>
-    //         <div className={styles.inputField}>
-    //             <p>Name:</p>
-    //             <input placeholder={faker.name.firstName()} />
-    //         </div>
-    //         <button>Generate</button>
-    //     </div>
-    // )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {DataGenerator} from "./DataGenerator/DataGenerator.tsx";
+import { DataGenerator } from './DataGenerator/DataGenerator.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
